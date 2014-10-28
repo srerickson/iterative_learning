d6a39bbe129f6215cb8de221200ad393b7e6d487
Seth Erickson <sr.erickson@gmail.com>
2014-10-27 21:53:00 -0700

    sum of error fitness function

--- lib/il/function_learning.rb
+++ lib/il/function_learning.rb
@@ -50,6 +50,26 @@ module IterativeLearning
     IterativeLearning.register_condition_builder('FUNC_NONLINEAR',self.method(:nonlinear))
 
 
+    def self.sum_of_error(target, trial)
+      if target.length != trial.length
+        raise "target and trial do not have same length"
+      end
+      target = IterativeLearning.departition_values(target) if target.is_a? Hash
+      trial = IterativeLearning.departition_values(trial) if trial.is_a? Hash
+      [target, trial].each{ |list| list.sort!{ |a,b| a[:x] <=> b[:x] }}
+      err = 0
+      (0..target.length-1).each do |i|
+        a = target[i].with_indifferent_access
+        b = trial[i].with_indifferent_access
+        if (i != a[:x]-1 ) and (i != b[:x]-1)
+          raise "target and trial values do not correspond"
+        end
+        err += (a[:y] - b[:y]).abs
+      end
+      err
+    end
+
+    IterativeLearning.register_fitness_function('FUNC_SUM_OF_ERR', self.method(:sum_of_error))
 
   end
 
