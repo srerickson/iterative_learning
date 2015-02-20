24c0d218a9595ec0d38168a73945d5d37b9f37af
Seth Erickson <sr.erickson@gmail.com>
2015-02-19 18:09:46 -0800

    work on e2e test

--- frontend/test/e2e/test_test.js
+++ frontend/test/e2e/test_test.js
@@ -1,11 +1,85 @@
-describe('iterative learning experiment admin page', function() {
-  it('should load', function() {
+describe('iterative learning', function() {
+  it('full run', function() {
 
     var request = require('sync-request');
+
     var resp = request('GET', 
-      "http://localhost:3000/experiment?key=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHBlcmltZW50X25hbWUiOiJyb3VuZDFfZmViMjAxNSJ9.P8h4khtwcA9Yd8rLDlq8A85ZaRcnH1Pp55uw2_KjeQI")
+      "http://localhost:3000/experiment?key=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHBlcmltZW50X25hbWUiOiJleGFtcGxlX2Jhc2ljIn0.X2LYqKkWFtyFOoMJ33PstxjHYDMGlLgIPf1dotnhjck")
     var experiment = JSON.parse(resp.getBody('utf8'));
-    console.log(experiment);
+
+    for(i in experiment.conditions){
+      var condition = experiment.conditions[i]
+      for(j in condition.chains){
+        var chain = condition.chains[j]
+        for(k in chain.generations){
+          var generation = chain.generations[k]
+          for(l in generation.tasks){
+
+            var task = generation.tasks[l]
+            task_url = "http://localhost:3000/#/task?key=" + task._jwt_key
+
+            // Intro Instructions
+            browser.get(task_url)
+            expect(browser.getCurrentUrl()).toEqual(task_url)
+            element(by.css("[ui-sref='task.training']")).click()
+
+            // Testing Page
+            expect(element(by.css('.feedback .fill')).isPresent()).toBe(true)
+            browser.waitForAngular();
+
+            for(val in task._start_values.training){
+
+              // A guess .. 
+              browser.driver.actions()
+                .mouseDown(element(by.css(".ui-slider-handle")))
+                .mouseMove({x:0, y: -10})
+                .mouseUp()
+                .perform()
+              element(by.css('[ng-click="next()"]')).click()
+
+              // check if correction is required
+              element(by.css('.feedback .fill')).getSize().then(
+                function(dims){
+                  var y = dims.height
+                  if(y > 0){ // guess was wrong, need to correct
+                    browser.driver.actions()
+                      .mouseDown(element(by.css(".ui-slider-handle")))
+                      .mouseMove({x:0, y: y*-1})
+                      .mouseUp()
+                      .perform()
+                    element(by.css('[ng-click="next()"]')).click()
+                  }
+                }, function(err){}
+              )
+            }
+
+            // Training Intro
+            expect(element(by.css('[ui-sref="task.testing"]')).isPresent()).toBe(true)
+            element(by.css('[ui-sref="task.testing"]')).click()
+
+            // Testing Phase
+            browser.waitForAngular();
+            for(val in task._start_values.testing){
+              guess = Math.ceil(Math.random() * 350)
+              browser.driver.actions()
+                .mouseDown(element(by.css(".ui-slider-handle")))
+                .mouseMove({x:0, y: guess*-1})
+                .mouseUp()
+                .perform()
+              element(by.css('[ng-click="next()"]')).click()
+            }
+
+
+            // Final
+            expect(element(by.css('h4')).isDisplayed()).toBeTruthy();
+
+
+          }
+        }
+      }
+
+    }
+
 
     // browser.get('http://localhost:3000/#/experiment?key=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHBlcmltZW50X25hbWUiOiJyb3VuZDFfZmViMjAxNSJ9.P8h4khtwcA9Yd8rLDlq8A85ZaRcnH1Pp55uw2_KjeQI');
 
--- frontend/test/protractor.conf.js
+++ frontend/test/protractor.conf.js
@@ -1,5 +1,12 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['e2e/*_test.js'],
-  baseUrl: 'http://localhost:9001' //default test port with Yeoman
+  baseUrl: 'http://localhost:9001', //default test port with Yeoman
+  jasmineNodeOpts: {
+    onComplete: null,
+    isVerbose: true,
+    showColors: true,
+    includeStackTrace: true,
+    defaultTimeoutInterval: 360000
+  }
 }
